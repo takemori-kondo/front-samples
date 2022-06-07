d039b8083c8735c8dabe2e44b1f73bced4336011
DESKTOP-BVING6D\Ether <etherealgarden@msn.com>
2022-06-08 01:22:00 +0900

    front03 package.json作り直し

---
+++ front03/.eslintrc.js
@@ -0,0 +1,21 @@
+/* eslint-env node */
+module.exports = {
+  "env": {
+    "browser": true,
+    "es2021": true,
+    "jquery": true,
+  },
+  "plugins": [
+    "html"
+  ],
+  "settings": {
+    "html/report-bad-indent": "error",
+  },
+  "extends": "eslint:recommended",
+  "parserOptions": {
+    "ecmaVersion": "latest"
+  },
+  "rules": {
+    "indent": ["warn", 2],
+  }
+}
